0b724eba6a3ae376aadd743d44e1815fc2341f7d
Jens van Almsick <bin101@users.noreply.github.com>
2023-12-30 16:47:54 +0100

    fix lint error

--- src/ffmpeg/ffmpeg.go
+++ src/ffmpeg/ffmpeg.go
@@ -246,13 +246,11 @@ func getTargetHost(config *config.Config, proc *processor.Processor) (processor.
 						Str("command", strings.Join(testFullCommand, " ")).
 						Msg("marking as bad")
 
-					proc.AddState(
-						processor.State{
-							HostId:    hostMapping.Id,
-							ProcessId: config.Program.Pid,
-							State:     "bad",
-						}
-					)
+					proc.AddState(processor.State{
+						HostId:    hostMapping.Id,
+						ProcessId: config.Program.Pid,
+						State:     "bad",
+						})
 					return
 				}
 				log.Debug().Msg("ssh test succeeded")
